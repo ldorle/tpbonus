9d091759dda562e03e9f426bc328ff3c59e066b3
ldorle <lucasdorle@hotmail.fr>
2022-07-07 12:39:23 +0200

    add css on Form Produits

--- src/Controller/ProduitsController.php
+++ src/Controller/ProduitsController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Produits;
 use App\Form\ProduitsType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,13 +18,32 @@ class ProduitsController extends AbstractController
   /**
    * @Route("/create", name="create")
    */
-  public function create(): Response
+  public function create(Request $request, EntityManagerInterface $entityManager): Response
   {
     $produits = new Produits();
+    $produits->setPublished(true);
+    $produits->setDateAdd(new \DateTime());
+    $produits->setDateEdit(new \DateTime());
+
     $produitsForm = $this->createForm(ProduitsType::class, $produits);
+
+    $produitsForm->handleRequest($request);
+
+    if ($produitsForm->isSubmitted() && $produitsForm->isValid()) {
+      $entityManager->persist($produits);
+      $entityManager->flush();
+      return $this->redirectToRoute('produits_details');
+    }
 
     return $this->render('produits/create.html.twig', [
       'produitsForm' => $produitsForm->createView()
     ]);
+  }
+
+  /**
+   * @Route("/details", name="details")
+   */
+  public function details() {
+    return $this->render('produits/details.html.twig');
   }
 }
